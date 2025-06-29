8837754ed7e0c1a16055d686c19bcc854a47e3b0
HuuLinh25 <huulinh250702@gmail.com>
2025-06-29 10:51:00 +0700

    GraphQL endpoints

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,6 +6,11 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { PostModule } from './post/post.module';
+import { TagModule } from './tag/tag.module';
+import { LikeModule } from './like/like.module';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,7 +19,12 @@ import { PostModule } from './post/post.module';
       autoSchemaFile: join(process.cwd(), "src/graphql/schema.gql"),
     }),
     PrismaModule,
-    PostModule],
+    PostModule,
+    TagModule,
+    LikeModule,
+    UserModule,
+    CommentModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
