3e01057e1b6850bc26c24649bb82988c01a3ff50
HuuLinh25 <huulinh250702@gmail.com>
2025-07-24 23:04:35 +0700

    protecting GraphQL endpoint

--- apps/api/src/auth/auth.resolver.ts
+++ apps/api/src/auth/auth.resolver.ts
@@ -1,12 +1,13 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { SignInInput } from './dto/signin.input';
+import { AuthPayload } from './entities/auth-payload.entities';
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
-  @Mutation(() => String)
+  @Mutation(() => AuthPayload)
   async signIn (@Args("signInInput") sinInInput: SignInInput){
     const user = await this.authService.validateLocalUser(sinInInput);
     return await this.authService.login(user);
