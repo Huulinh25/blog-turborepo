4632e51f3f1c7eafee40ea0ee269f70da45adb1e
Huulinh2507 <huulinh250702@gmail.com>
2025-07-30 17:58:22 +0700

    update code 30/7

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -8,41 +8,55 @@ import { User } from 'generated/prisma';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService, private jwtService: JwtService) {
-        
-    }
-    
-    async validateLocalUser({ email, password }:SignInInput) {
-        const user = await this.prisma.user.findUnique({
-            where: {
-                email
-            },
-        });
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-        if(!user) throw new UnauthorizedException('User not found');
+  async validateLocalUser({ email, password }: SignInInput) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        const passswordMatches = await verify(user.password, password);
+    if (!user) throw new UnauthorizedException('User not found');
 
-        if(!passswordMatches)
-            throw new UnauthorizedException('Invalid Credentials!');
+    const passswordMatches = await verify(user.password, password);
 
-        return user;
-    }
+    if (!passswordMatches)
+      throw new UnauthorizedException('Invalid Credentials!');
 
-    async generateToken(userId: number) {
-        const payload: AuthJwtPayload = { sub: userId };
-        const accessToken = await this.jwtService.signAsync(payload);
-        return accessToken;
-    }
+    return user;
+  }
 
-    async login (user: User) {
-        const accessToken = await this.generateToken(user.id);
+  async generateToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return accessToken;
+  }
 
-        return {
-            id: user.id,
-            name: user.name,
-            avatar: user.avatar,
-            accessToken
-        }
-    }
+  async login(user: User) {
+    const accessToken = await this.generateToken(user.id);
+
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
+  }
+
+  async validateJwtUser(userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('User not found');
+    const currentUser = { id: user.id };
+
+    return currentUser;
+  }
 }
