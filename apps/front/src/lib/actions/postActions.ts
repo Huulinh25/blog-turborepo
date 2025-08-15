ca2f7dfab653b26eb74a81f1fd80f80270429ddb
Huulinh25 <huulinh250702@gmail.com>
2025-08-16 00:01:30 +0700

    Pagination in Home Page

--- apps/front/src/lib/actions/postActions.ts
+++ apps/front/src/lib/actions/postActions.ts
@@ -4,9 +4,18 @@ import { print} from "graphql";
 import { fetchGraphQL } from "../fetchGraphQL"
 import { GET_POSTS } from "../gqlQueries";
 import { Post } from "../types/modelTypes";
+import { transformTakeSkip } from "../helpers";
 
-export const fetchPosts = async () => {
-    const data = await fetchGraphQL(print(GET_POSTS));
+export const fetchPosts = async ({
+    page,
+    pageSize
+    }:{
+        page?: number;
+        pageSize?: number
+    }) => {
+    const {skip, take} = transformTakeSkip({page, pageSize});
+
+    const data = await fetchGraphQL(print(GET_POSTS), {skip, take});
     // console.log({data});
-    return data.posts as Post[];
+    return {posts: data.posts as Post[], totalPosts: data.pageCount};
 }
