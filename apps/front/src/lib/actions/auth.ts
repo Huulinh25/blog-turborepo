8d393bd11417200b18520ee6290f58d124de9d9b
Huulinh25 <huulinh250702@gmail.com>
2025-08-16 20:07:20 +0700

    SignIn Page

--- apps/front/src/lib/actions/auth.ts
+++ apps/front/src/lib/actions/auth.ts
@@ -4,8 +4,9 @@ import { SignUpFormState } from "@/lib/types/formState";
 import { SignUpFormSchema } from "../ZodSchemas/signUpFormSchema";
 import { fetchGraphQL } from "../fetchGraphQL";
 import { print } from "graphql";
-import { CREATE_USER_MUTATION } from "../gqlQueries";
+import { CREATE_USER_MUTATION, SIGN_IN_MUTATION } from "../gqlQueries";
 import { redirect } from "next/navigation";
+import { LoginFormSchema } from "../ZodSchemas/loginFormSchema";
 
 export async function signUp(
   state: SignUpFormState,
@@ -34,4 +35,33 @@ export async function signUp(
   }
 
   redirect("/auth/signin");
+}
+
+export async function signIn(
+  state: SignUpFormState,
+  formData: FormData
+): Promise<SignUpFormState> {
+  const validatedFields = LoginFormSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+
+  if (!validatedFields.success)
+    return {
+      data: Object.fromEntries(formData.entries()),
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+
+  const data = await fetchGraphQL(print(SIGN_IN_MUTATION), {
+    input: {
+      ...validatedFields.data,
+    },
+  });
+
+  if (data.errors) {
+    return {
+      data: Object.fromEntries(formData.entries()),
+      message: "Invalid Credentials",
+    };
+  }
+  redirect("/");
 }
